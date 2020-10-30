3c670cf35736311f540739aa432410efd2f9b9e2
胡逸雯 <huyiwen@huyiwendeMacBook-Pro-3.local>
2020-10-30 12:07:22 +0800

    new progress! Almost done except for the text split!

--- background.js
+++ background.js
@@ -4,80 +4,103 @@ console.log("this is background script");
 // get all active tabs in window
 // chrome.tabs.query({active:true,windowType:"normal", currentWindow: true},function(d){console.log(d[0].id);})
 let historyList = [];
+let historyVisit = [];
+
+// chrome.history.search({ text: '' }, function(data) {
+//     data.forEach(function(page) {
+//         historyVisit.push(page)
+//     });
+// });
+
 
 //when a new website has been visited
 chrome.history.onVisited.addListener(function(data) {
-    // historyList.push(data.url);
-    // console.log(historyList);
+  historyVisit.push(data)
+  console.log(data.visitCount)
+  console.log(historyVisit)
 
-    // the following is for getting window scroll position of the active tab (as well as tab ID)
-    setTimeout(() => {
+  // the following is for getting window scroll position of the active tab
+  setTimeout(() => {
     //when tab is active
-        chrome.tabs.query({
-          active: true,
-          currentWindow: true
-        }, function(tabs) {
+    chrome.tabs.query({
+      active: true,
+      currentWindow: true
+    }, function(tabs) {
 
-          //get tab ID
-          let tabID = tabs[0].id;
-          console.log("tab id is " + tabID)
+    //visit count
+      let visitCount = {
+        historyVisit: historyVisit,
+        thisPageVisitCount: data.visitCount
+      }
+      chrome.tabs.sendMessage(tabs[0].id, visitCount);
+      console.log(visitCount);
 
-          //when bookmark is clicked, receive button click message from popup script
-          chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) {
-
-            // console.log(message.type);
-            if (message.type = "getTabId"){
-              //send tabID to content script
-              //(tabs.sendmessage: specific for sending message to content script)
-              chrome.tabs.sendMessage(tabs[0].id, tabID
-                //get response: scrolling position
-                ,(response)=>{
-                console.log("X: " + response.scrollX + " , "+ "Y: " + response.scrollY);
-
-                //store window Y position and urls
-                var windowY = response.scrollY;
-                console.log(windowY);
-
-                //create a pair for website url and position
-                let websiteInfo = {
-                  windowY:windowY,
-                  storedURL:data.url
-                }
-
-                historyList.push(websiteInfo);
-                console.log(historyList);
-
-                chrome.storage.local.set(websiteInfo, function() {
-                  console.log("windowY position and url are stored at", windowY,historyList);
-                });
-
-                //get window Y position and urls
-                    chrome.storage.local.get(websiteInfo, function(result) {
-                      console.log("get window position and url");
-                      console.log(websiteInfo);
-                    })
-
-                  let url = websiteInfo.storedURL;
-                  url.onload = ()=>{
-                    console.log("this page has been revisited");
-                  }
-
-
-              })
-
-            }
-
-
+      //history websites info
+      for (let i = 0; i < historyList.length; i++) {
+        if (tabs[0].url == historyList[i].storedURL) {
+          console.log('this website is revisited')
+          chrome.tabs.sendMessage(tabs[0].id, {
+            bookmarkPos: "bookmarkPos",
+            posY: historyList[i].windowY,
           })
-        })
+        }
+      }
 
-      }, 1000); //Without setTimeout, it will send the request too fast that the tab can't receive it
+      //when bookmark is clicked, receive button click message from popup script
+      chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) {
 
+        // console.log(message.type);
+        if (message.type = "getTabId") {
+          //send tabID to content script
+          //(tabs.sendmessage: specific for sending message to content script)
+          chrome.tabs.sendMessage(tabs[0].id,tabs[0].id
+            //get response: scrolling position
+            , (response) => {
+              console.log("X: " + response.scrollX + " , " + "Y: " + response.scrollY);
+
+              //store window Y position and urls
+              var windowY = response.scrollY;
+              console.log(windowY);
+
+              //create a pair for website url and position
+              let websiteInfo = {
+                windowY: windowY,
+                storedURL: data.url
+              }
+
+              let revisit = false;
+
+              for (let i = 0; i < historyList.length; i++) {
+                if (historyList[i].storedURL == data.url) {
+                  historyList[i].windowY = windowY
+                  revisit = true
+                }
+              }
+
+              if (revisit == false) {
+                historyList.push(websiteInfo);
+              }
+
+              console.log(historyList);
+
+              historyList.push(websiteInfo);
+              console.log(historyList);
+
+              chrome.storage.local.set(websiteInfo, function() {
+                console.log("windowY position and url are stored at", windowY, historyList);
+              });
+
+              //get window Y position and urls
+              chrome.storage.local.get(websiteInfo, function(result) {
+                console.log("get window position and url");
+                console.log(websiteInfo);
+              })
+            })
+        }
+
+      })
     })
 
+  }, 1000); //Without setTimeout, it will send the request too fast that the tab can't receive it
 
-
-
-      //     console.log(scrollPosition);
-
-      //
+})
--- contentscript.js
+++ contentscript.js
@@ -5,8 +5,8 @@ console.log("Hi I'm content script!");
 
   chrome.runtime.onMessage.addListener(
     function(message, sender, sendResponse) {
-      //get TabID
-      console.log(message);
+     console.log(message);
+      //get scroll position
       if(message.type = "getScrollPosition"){
           //get scroll position
           var x = window.scrollX;
@@ -21,57 +21,181 @@ console.log("Hi I'm content script!");
       console.log(scrollPosition);
       sendResponse(scrollPosition);
     }
+
+    //get bookmark information
+    if (message.bookmarkPos == "bookmarkPos") {
+            console.log(`scroll to bookmark position ${message.posY}`)
+            window.scrollTo(0, message.posY)
+        }
+
+     //get page visit information
+     console.log("this page has been visited" + message.thisPageVisitCount + "times");
+
+     if (message.thisPageVisitCount > 3) {
+         let tagNames = ['div', 'header', 'section', 'main', 'pre', 'table', 'p', 'h3', 'h2', 'h1', 'li']
+         for (let i = 0; i < tagNames.length; i++) {
+             let thisAllTag = document.getElementsByTagName(tagNames[i]);
+             for (let j = 0; j < thisAllTag.length; j++) {
+                 thisAllTag[j].style.backgroundColor = "transparent";
+                 thisAllTag[j].style.backgroundImage = "none";
+             }
+         }
+
+         if (message.thisPageVisitCount > 4){
+           //bookworm1
+           let windowHeight = window.innerHeight;
+           let windowWidth = window.innerWidth;
+           let bookworm1 = document.createElement("img");
+           bookworm1.src = chrome.extension.getURL("images/bookworm1.gif")
+           bookworm1.style.width = 30 + Math.random()*100 + "px";
+           bookworm1.style.position = "absolute";
+           bookworm1.style.left = 50 + Math.random()*(windowWidth-50) + "px";
+           bookworm1.style.top = 50 +  Math.random()*(windowHeight-50) + "px";
+           document.body.appendChild(bookworm1);
+
+         }
+
+
+         if(message.thisPageVisitCount > 6){
+           //bookworm2
+           let windowHeight = window.innerHeight;
+           let windowWidth = window.innerWidth;
+           let bookworm2 = document.createElement("img");
+           bookworm2.src = chrome.extension.getURL("images/bookworm2.gif")
+           bookworm2.style.width = 10 + Math.random()*100 + "px";
+           bookworm2.style.position = "absolute";
+           bookworm2.style.left = 50 + Math.random()*(windowWidth-50) + "px";
+           bookworm2.style.top = 50 +  Math.random()*(windowHeight-50) + "px";
+           document.body.appendChild(bookworm2);
+
+         }
+
+
+        if(message.thisPageVisitCount > 8){
+          //bookworm3
+          let windowHeight = window.innerHeight;
+          let windowWidth = window.innerWidth;
+          let bookworm3 = document.createElement("img");
+          bookworm3.src = chrome.extension.getURL("images/bookworm3.gif")
+          bookworm3.style.width = 30 + Math.random()*100 + "px";
+          bookworm3.style.position = "absolute";
+          bookworm3.style.left = 50 + Math.random()*(windowWidth-50) + "px";
+          bookworm3.style.top = 50 +  Math.random()*(windowHeight-50) + "px";
+          document.body.appendChild(bookworm3);
+
+        }
+
+         if (message.thisPageVisitCount < 10) {
+             document.body.style.backgroundImage = "url('https://i.pinimg.com/564x/11/29/36/112936939ca1abe3c4615adeeeb42cfb.jpg')"
+         } else if (message.thisPageVisitCount < 20) {
+             document.body.style.backgroundImage = "url('https://i.pinimg.com/564x/f0/18/8a/f0188a4680b2ef1ef813b9602b990798.jpg')"
+
+         } else {
+             document.body.style.backgroundImage = "url('https://i.pinimg.com/474x/28/10/d6/2810d6f8b870137056dbfaf7645506ba.jpg')"
+         }
+     }
+
+     //text cut
+     let allpTags = document.getElementsByTagName('p')
+     for (let i = 0; i < allpTags.length; i++) {
+         let thisText = allpTags[i].innerHTML;
+         let thisTextLength = thisText.length;
+         let thisLostLetterNum = Math.floor(mapRange(thisTextLength, 0, 200, 0, 50));
+         for (let n = 0; n < thisLostLetterNum; n++) {
+             let lostLetter = Math.floor(Math.random() * thisTextLength);
+             // console.log(thisText[lostLetter])
+             thisText = thisText.replaceAt(lostLetter, `\u00A0`)
+             // console.log(thisText)
+         }
+         allpTags[i].innerText = thisText
+     }
+
+
+ // chrome.runtime.onMessage.addListener(gotMessage);
+
+ function mapRange(value, a, b, c, d) { //Simulating the map function in p5.js
+     value = (value - a) / (b - a);
+     return c + value * (d - c);
+ }
+
+ String.prototype.replaceAt = function(index, replacement) {
+     if (index >= this.length) {
+         return this.valueOf();
+     }
+
+     var chars = this.split('');
+     chars[index] = replacement;
+     return chars.join('');
+  }
+
+
 })
 
 
-
-
-
-
-// let tagNames = ['div', 'header', 'section']
-// for (let i = 0; i < tagNames.length; i++) {}
 //
-// document.body.style.backgroundImage = "url('https://i.pinimg.com/564x/f0/18/8a/f0188a4680b2ef1ef813b9602b990798.jpg')"
-// let allDivs = document.getElementsByTagName('div');
-// let allHeaders = document.getElementsByTagName('header')
-// let allSections = document.getElementsByTagName('section')
-// let allMains = document.getElementsByTagName('main')
-// let allPres = document.getElementsByTagName('pre')
-// let allTables = document.getElementsByTagName('table')
-// let allPs = document.getElementsByTagName('p')
-// let allh3s = document.getElementsByTagName('h3')
+// function gotMessage(message, sender, sendResponse) {
+//     console.log(message.historyVisit);
+//
+//     if (message.thisPageVisitCount > 3) {
+//         let tagNames = ['div', 'header', 'section', 'main', 'pre', 'table', 'p', 'h3', 'h2', 'h1', 'li']
+//         for (let i = 0; i < tagNames.length; i++) {
+//             let thisAllTag = document.getElementsByTagName(tagNames[i]);
+//             for (let j = 0; j < thisAllTag.length; j++) {
+//                 thisAllTag[j].style.backgroundColor = "transparent";
+//                 thisAllTag[j].style.backgroundImage = "none";
+//
+//             }
+//         }
+//
+//         if (message.thisPageVisitCount > 4){
+//           //bookworm1
+//           let bookworm1 = document.createElement("img");
+//           bookworm1.src = chrome.extension.getURL("images/bookworm1.gif")
+//           document.body.appendChild(bookworm1);
+//           bookworm1.style.width = "100px";
+//         }
 //
 //
-// console.log(allTables)
+//         if (message.thisPageVisitCount < 10) {
+//             document.body.style.backgroundImage = "url('https://i.pinimg.com/564x/11/29/36/112936939ca1abe3c4615adeeeb42cfb.jpg')"
+//         } else if (message.thisPageVisitCount < 20) {
+//             document.body.style.backgroundImage = "url('https://i.pinimg.com/564x/f0/18/8a/f0188a4680b2ef1ef813b9602b990798.jpg')"
 //
-// for (let i = 0; i < allDivs.length; i++) {
-//     allDivs[i].style.backgroundColor = "transparent"
+//         } else {
+//             document.body.style.backgroundImage = "url('https://i.pinimg.com/474x/28/10/d6/2810d6f8b870137056dbfaf7645506ba.jpg')"
+//         }
+//     }
+//
+//     let allpTags = document.getElementsByTagName('p')
+//     for (let i = 0; i < allpTags.length; i++) {
+//         let thisText = allpTags[i].innerText
+//         let thisTextLength = thisText.length
+//         let thisLostLetterNum = Math.floor(mapRange(thisTextLength, 0, 200, 0, 50));
+//         for (let n = 0; n < thisLostLetterNum; n++) {
+//             let lostLetter = Math.floor(Math.random() * thisTextLength);
+//             // console.log(thisText[lostLetter])
+//             thisText = thisText.replaceAt(lostLetter, `\u00A0`)
+//             // console.log(thisText)
+//         }
+//         allpTags[i].innerText = thisText
+//     }
+//
+//
+// chrome.runtime.onMessage.addListener(gotMessage);
+//
+// function mapRange(value, a, b, c, d) { //Simulating the map function in p5.js
+//     value = (value - a) / (b - a);
+//     return c + value * (d - c);
 // }
 //
-// for (let i = 0; i < allHeaders.length; i++) {
-//     allHeaders[i].style.backgroundColor = "transparent"
+// String.prototype.replaceAt = function(index, replacement) {
+//     if (index >= this.length) {
+//         return this.valueOf();
+//     }
+//
+//     var chars = this.split('');
+//     chars[index] = replacement;
+//     return chars.join('');
 // }
 //
-// for (let i = 0; i < allSections.length; i++) {
-//     allSections[i].style.backgroundColor = "transparent"
-// }
-//
-// for (let i = 0; i < allMains.length; i++) {
-//     allMains[i].style.backgroundColor = "transparent"
-// }
-//
-// for (let i = 0; i < allPres.length; i++) {
-//     allPres[i].style.backgroundColor = "transparent"
-// }
-//
-// for (let i = 0; i < allTables.length; i++) {
-//     allTables[i].style.backgroundColor = "transparent"
-// }
-//
-// for (let i = 0; i < allPs.length; i++) {
-//     allPs[i].style.backgroundColor = "transparent"
-// }
-//
-// for (let i = 0; i < allh3s.length; i++) {
-//     allh3s[i].style.backgroundColor = "transparent"
 // }
